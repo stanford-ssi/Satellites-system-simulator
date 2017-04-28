f6c85746253ee84f938a5856da63851a659b7a38
Jake Hillard <jhillard3.14@gmail.com>
2017-04-28 13:24:35 -0700

    Angle sweep is sweeping. Compute dX with noise powers

--- quad_block.m
+++ quad_block.m
@@ -17,32 +17,27 @@ function quad_outputs = quad_block(thetas, sig_power, spot_radius, bg_power, noi
     I_bg = 0;%Background intensity. However you scale that. 
     
     pixel_gap = 30E-6; %30um;
-    %pixel_gap = 30E-5;
     quad_width = 0.01; %10mm 
     %Assumes square quad
 %%
 %Section 1.9999:
 %Simulate power over all possible X,Y positions and return
 %cordinate estimates for each. 
-% low_x = -quad_width/2;
-% high_x = quad_width/2;
-% low_y = -quad_width/2;
-% high_y = quad_width/2;
 low_x = -1;
 high_x = 1;
 low_y = -1;
-high_y = 1;
-num_locations = 2;  %Number of different spot positions to test. scales N^2
+high_y = 1; %Cordinates are normalized to width of quad/2.
+num_locations = 30;  %Number of different spot positions to test. scales N^2
 simulation_depth = 100; %How many points to integrate over for power calcs. Scales N^2
 diff = high_x - low_x;
 [spot_pos_x, spot_pos_y]  = meshgrid(low_x:diff/num_locations:high_x,low_y:diff/num_locations:high_y);           
-x_hat = zeros(num_locations);
+x_hat = zeros(num_locations);x
 y_hat = zeros(num_locations);
 
 
 for p = spot_pos_x(1,:)
     for m = spot_pos_y(:,1)'
-            figure
+            
             spot_x = p; %01;%Ranges from -1 to 1.
             spot_y = m;
             n = simulation_depth;
@@ -90,15 +85,23 @@ for p = spot_pos_x(1,:)
             q_sum = sum(sum(quad));
             Y_hat = ((quad(1,1)+quad(2,1)) - (quad(1,2)+quad(2,2))) / q_sum;
             X_hat = ((quad(1,1)+quad(1,2)) - (quad(2,1)+quad(2,2))) / q_sum;
-            P = round( p*num_locations/quad_width+num_locations/2+1 );
-            M = round( m*num_locations/quad_width+num_locations/2+1 );
+            P = round( p*num_locations/2+num_locations/2+1 );
+            M = round( m*num_locations/2+num_locations/2+1 );
             x_hat(P,M) = X_hat;     
             y_hat(P,M) = Y_hat;
     end
 end
-figure
-surf(spot_pos_x, spot_pos_y, x_hat);
-surf(spot_pos_x, spot_pos_y, y_hat);
+
+if(verbose)
+    figure
+    surf(spot_pos_x, spot_pos_y, x_hat);
+    hold on;
+    surf(spot_pos_x, spot_pos_y, y_hat);
+    title('Position Estimates VS True Estimates');
+    xlabel('True X position (Normalized to Width of Detector)');
+    ylabel('True Y Position (Normalized to Width of Detector)');
+    zlabel('Predicted Position');
+end
 
 
     
